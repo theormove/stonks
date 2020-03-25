2214711ff20b0f6459e2cfd3c77112da045418e2
THEORMOVE <theormove@gmail.com>
2020-03-25 03:06:33 +0200

    subs

--- stonks/users/models.py
+++ stonks/users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	subscription_plan = models.CharField(max_length = 100, default = "NONE", blank = True)
 	categories = models.TextField()
 	recieve_source = models.TextField()
 	category = models.BooleanField(default = True)
--- stonks/users/views.py
+++ stonks/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, ProfileUpdateFrom
+from django.forms.models import model_to_dict
 
 def register(request):
 	if request.method == 'POST':
@@ -19,9 +20,35 @@ def register(request):
 def profile(request):
 	if request.method == 'POST':
 		form = ProfileUpdateFrom(request.POST, instance = request.user.profile)
-		if form.is_valid():
+		if form.is_valid() and request.user.profile.subscription_plan == "CATEGORIES":
 			form.save()
+			return redirect('profile')
+		else:
+			messages.add_message(request, messages.ERROR, 'Change your subscription plan to do that')
 			return redirect('profile')
 	else:
 		form = ProfileUpdateFrom(instance = request.user.profile)		
 	return render(request, 'users/profile.html',{'form':form})
+
+
+@login_required
+def subscriptions(request):
+	if request.user.profile.subscription_plan == 'NONE':
+		btn = "Subscribe"
+	else:		
+		btn = "Cancel"	
+	if request.method == 'POST':
+		if "Subscribe" in request.POST:
+			request.user.profile.subscription_plan = "CATEGORIES"
+			request.user.profile.save()
+		else:
+			request.user.profile.subscription_plan = "NONE"
+			data = model_to_dict(request.user.profile)
+			for field in data:
+				if data[field] == False:
+					setattr(request.user.profile, field, True)
+			request.user.profile.save()	
+		return redirect('subscriptions')
+	buttons = {'btn':btn}	
+	return render(request, 'users/subscriptions.html', buttons)
+			
