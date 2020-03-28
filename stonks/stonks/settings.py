367d8c956e8bebbfc6284c0ff2e66c7aa154deb2
THEORMOVE <theormove@gmail.com>
2020-03-28 02:05:48 +0200

    h2

--- stonks/stonks/settings.py
+++ stonks/stonks/settings.py
@@ -129,6 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
