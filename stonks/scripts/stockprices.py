cb8d303232f6f801c91eb588da07f4816201db4c
THEORMOVE <theormove@gmail.com>
2020-04-05 03:08:34 +0300

    yahoopricedemo))

--- stonks/scripts/stockprices.py
+++ stonks/scripts/stockprices.py
@@ -11,17 +11,47 @@ from API.models import StockPrice
 
 import requests 
 import json
+import requests
+import bs4
+import datetime 
+from bs4 import BeautifulSoup
 
-with open('KEY.txt', 'r') as f:
-	key = f.read()
-while True:
+stockdata1 = {"Meta Data":{},
+			"Time Series(5min)":{}}
+			
+
+def yahoo_graph(stockdataobj, link = "https://finance.yahoo.com/quote/RUB=X?p=RUB=X&.tsrc=fin-srch"):
+	try:	
+		r = requests.get(link)
+		time = datetime.datetime.today()
+		soup = BeautifulSoup(r.text, features="html.parser")
+		value = soup.find_all("div", class_ = "My(6px) Pos(r) smartphone_Mt(6px)")[0].find('div').find('span').text 
+		stockdataobj.data["Time Series(5min)"][str(time)] = {'open': value,
+												'high': value,
+												'low': value,
+												'close': value,	
+												'volume': '100',}
+		print(stockdataobj.name)
+		stockdataobj.save()
+	except Exception as e:
+				print(e.__doc__)	
+
+def alpha_graph():
+	with open('KEY.txt', 'r') as f:	
+		key = f.read()
 	try:
 		msft = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey={}'.format(key))
 		obj = StockPrice.objects.get(name='MSFT')
-		obj.data = msft.text
+		obj.data = json.loads(msft.text)
 		obj.save()
 		print('Successfully added data to MSFT')
 	except Exception as e:
 		print(e.__doc__)
-		print(e.message)
-	time.sleep(300)	
+
+def run():
+	while True:
+		alpha_graph()
+		yahoo_graph(StockPrice.objects.get(name='RUB/USD'))
+		time.sleep(300)
+
+run()			
